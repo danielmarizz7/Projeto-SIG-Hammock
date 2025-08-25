e80259554ea0c1b9a635a98c5e11a6e059901c33
danielmarizz7 <daniel.mariz.700@ufrn.edu.br>
2025-08-25 09:09:27 -0300

    Cria tela de cadastro de clientes

---
+++ clientes.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//assinaturas do módulo clientes
+void tela_de_clientes(void);
+void cadastrar_clientes(void);
+
+
+
+
+int main(void){
+    tela_de_clientes();
+    cadastrar_clientes();
+    return 0;
+}
+
+
+void tela_de_clientes(void){
+    char op_cliente;
+    system("clear || cls");
+    printf("╔═════════════════════════════════════════════════╗\n");
+    printf("║               Módulo De Clientes                ║\n");
+    printf("╠═════════════════════════════════════════════════╣\n");
+    printf("║ 1 - Cadastrar Cliente                           ║\n");
+    printf("║ 2 - Exibir Clientes                             ║\n");
+    printf("║ 3 - Editar Cliente                              ║\n");
+    printf("║ 4 - Excluir Cliente                             ║\n");
+    printf("║                                                 ║\n");
+    printf("║ 0 - Voltar                                      ║\n");
+    printf("╚═════════════════════════════════════════════════╝\n");
+    printf("\n");
+    printf("Pressione 0 para voltar à tela inicial: ");
+    scanf(" %c", &op_cliente);
+}
+
+void cadastrar_clientes(void){
+    char nome[51];
+    char cpf[12];
+    char email[26];
+    char telefone[12];
+    system("clear || cls");
+    printf("╔═════════════════════════════════════════════════╗\n");
+    printf("║               Cadastrar Clientes                ║\n");
+    printf("╚═════════════════════════════════════════════════╝\n");
+    printf("Digite o nome do cliente: ");
+    scanf(" %s", nome);
+    printf("Digite o CPF do cliente: ");
+    scanf(" %s", cpf);
+    printf("Digite o email do cliente: ");
+    scanf(" %s", email);
+    printf("Digite o telefone do cliente: ");
+    scanf(" %s", telefone);
+    printf("Cliente cadastrado com sucesso!\n");
+    printf("Nome: %s\n", nome);
+    printf("CPF: %s\n", cpf);
+    printf("Email: %s\n", email);
+    printf("Telefone: %s\n", telefone);
+}
