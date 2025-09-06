1970e65ae1bf1f8b77dbed53f22caa4f51cd5423
danielmarizz7 <daniel.mariz.700@ufrn.edu.br>
2025-09-06 12:57:53 -0300

    Adiciona tela de equipe no arquivo e o header

---
+++ equipe.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+
+
+void tela_de_equipe (void){
+    char op_equipe;
+    system("clear || cls");
+    printf("╔═══════════════════════════════════════════════════════════════════════╗\n");
+    printf("║                           SOBRE O PROJETO                             ║\n");
+    printf("╠═══════════════════════════════════════════════════════════════════════╣\n");
+    printf("║   Este projeto foi desenvolvido pelos alunos:                         ║\n");
+    printf("║      Daniel Mariz                                                     ║\n");
+    printf("║       GitHub: danielmarrizz7                                          ║\n");
+    printf("║      Gabriel Victor                                                   ║\n");
+    printf("║       GitHub: GabrielVictor7                                          ║\n");
+    printf("║      xxxxxxxxxxxxxx                                                   ║\n");
+    printf("║       GitHub: xxxxxxxx                                                ║\n");
+    printf("║                                                                       ║\n");
+    printf("║   Com a supervisão do Gerente                                         ║\n");
+    printf("║      Flavius Gorgonio                                                 ║\n");
+    printf("║       Github: FlaviusGorgonio                                         ║\n");
+    printf("║                                                                       ║\n");
+    printf("╚═══════════════════════════════════════════════════════════════════════╝\n");
+    printf("\n");
+    printf("Pressione enter para voltar à tela inicial: ");
+    scanf(" %c", &op_equipe);
+}
