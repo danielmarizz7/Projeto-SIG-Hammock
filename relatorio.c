d46b56ac4791a07fbebb023c3470757e2d003f6d
danielmarizz7 <daniel.mariz.700@ufrn.edu.br>
2025-09-08 13:44:11 -0300

    Altera módulo relatório para receber atualizações futuras

--- relatorio.c
+++ relatorio.c
@@ -1,47 +1,47 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "relatorio.h"
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include "relatorio.h"
 
 
 
-// void modulo_relatorio(void);
-//     char opcao;
+// // void modulo_relatorio(void);
+// //     char opcao;
 
-//     do {
-//         opcao = tela_de_rel();
-//         switch(opcao){
-//             case '1':   relatorio_clientes();
-//                         break;
-//             case '2':   relatorio_funcionarios();
-//                         break;
-//             case '3':   relatorio_produtos();
-//                         break;
-//             case '4':   quadro_estoque();
-//                         break;
+// //     do {
+// //         opcao = tela_de_rel();
+// //         switch(opcao){
+// //             case '1':   relatorio_clientes();
+// //                         break;
+// //             case '2':   relatorio_funcionarios();
+// //                         break;
+// //             case '3':   relatorio_produtos();
+// //                         break;
+// //             case '4':   quadro_estoque();
+// //                         break;
 
-//         }
-//     } while (opcao != '0');
+// //         }
+// //     } while (opcao != '0');
+// // }
+
+// char tela_relatorio(void){
+//     char op_relat;
+//     system("clear || cls");
+//     printf("╔═════════════════════════════════════════════════╗\n");
+//     printf("║                  Relatorio Geral                ║\n");
+//     printf("╠═════════════════════════════════════════════════╣\n");
+//     printf("║ 1 - Relatório geral de clientes                 ║\n");
+//     printf("║ 2 - Relatório geral de Funcionários             ║\n");
+//     printf("║ 3 - Relatório geral de Produtos                 ║\n");
+//     printf("║ 4 - Relatório geral de Estoque                  ║\n"); //ESSE CAMPO RECEBERÁ AS INFORMAÇÕES DA TELA DE "CONSULTAR ESTOQUE" E PASSARÁ INFORMAÇÕES PARA O CAMPO DE RELATÓRIO
+//     printf("║                                                 ║\n");
+//     printf("║ 0 - Voltar                                      ║\n");
+//     printf("╚═════════════════════════════════════════════════╝\n");
+//     printf("\n");
+//     printf("Pressione 0 para voltar à tela inicial: ");
+//     scanf(" %c", op_relat);
+//     getchar();
+
+//     //IDEIAS DE RELATÓRIO COM FILTROS :BUSCA POR SEXO
+//     //
 // }
-
-char tela_relatorio(void){
-    char op_relat;
-    system("clear || cls");
-    printf("╔═════════════════════════════════════════════════╗\n");
-    printf("║                  Relatorio Geral                ║\n");
-    printf("╠═════════════════════════════════════════════════╣\n");
-    printf("║ 1 - Relatório geral de clientes                 ║\n");
-    printf("║ 2 - Relatório geral de Funcionários             ║\n");
-    printf("║ 3 - Relatório geral de Produtos                 ║\n");
-    printf("║ 4 - Relatório geral de Estoque                  ║\n"); //ESSE CAMPO RECEBERÁ AS INFORMAÇÕES DA TELA DE "CONSULTAR ESTOQUE" E PASSARÁ INFORMAÇÕES PARA O CAMPO DE RELATÓRIO
-    printf("║                                                 ║\n");
-    printf("║ 0 - Voltar                                      ║\n");
-    printf("╚═════════════════════════════════════════════════╝\n");
-    printf("\n");
-    printf("Pressione 0 para voltar à tela inicial: ");
-    scanf(" %c", op_relat);
-    getchar();
-
-    //IDEIAS DE RELATÓRIO COM FILTROS :BUSCA POR SEXO
-    //
-}
 
