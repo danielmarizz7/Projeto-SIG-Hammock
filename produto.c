5a6e33ec6427c9030f14de4e419fa5508191ef41
danielmarizz7 <daniel.mariz.700@ufrn.edu.br>
2025-09-29 17:11:07 -0300

    Update: Atualiza a função fazendo com que ela passe a ler e salvar dados em arquivos .csv

--- produto.c
+++ produto.c
@@ -10,9 +10,6 @@
 FILE * arquivo_produto;
 
 
- 
-
-
 void modulo_produto(void){
 
     char opcao;
@@ -24,7 +21,7 @@ void modulo_produto(void){
     do {
         opcao = tela_de_produto();
         switch(opcao){
-            case '1':   cadastrar_produto();
+            case '1':   cadastrar_produto(nome, valor_produto, tipo_produto, cor_produto);
                         break;
             case '2':   exibir_produto();
                         break;
@@ -60,32 +57,54 @@ char tela_de_produto(void){
     return op_produtos;
 }
 
-void cadastrar_produto(void){
-    char nome_produto[55];
-    char id_produto[4];
-    char valor_produto[10];
-    char cor_produto[15];
+void cadastrar_produto(char nome[], char valor_produto[], char tipo_produto[], char cor_produto[]){
+    int id_produto = 0;
+    limpar_buffer();
     system("clear || cls");
     printf("╔═════════════════════════════════════════════════╗\n");
     printf("║                Cadastrar Produto                ║\n");
     printf("╚═════════════════════════════════════════════════╝\n");
-    printf("Digite o nome do produto: ");
-    fgets(nome_produto, sizeof(nome_produto), stdin);
-    printf("Digite o ID do produto: ");
-    fgets(id_produto, sizeof(id_produto), stdin);
-    printf("Digite um valor para o produto: ");
-    fgets(valor_produto, sizeof(valor_produto), stdin);
-    printf("Digite a cor do produto: ");
-    fgets(cor_produto, sizeof(cor_produto), stdin);
-    printf("\n");
-    printf("Produto cadastrado com sucesso!\n");
-    printf("\n");
-    printf("nome: %s\n", nome_produto);
-    printf("id: %s\n", id_produto);
-    printf("valor: %s\n", valor_produto);
-    printf("cor: %s\n", cor_produto);
-    getchar();
+
+    printf("Digite o nome do Produto: ");
+    scanf("%[^\n]", nome);
+    limpar_buffer();
+
+    printf("Digite o Valor: ");
+    scanf("%[^\n]", valor_produto);
+    limpar_buffer();
+
+    printf("Digite o Tipo: ");
+    scanf("%[^\n]", tipo_produto);
+    limpar_buffer();
+
+    printf("Digite a Cor: ");
+    scanf("%[^\n]", cor_produto);
+    limpar_buffer();
+
+    arquivo_produto = fopen("produtos.csv", "rt");
+
+    id_produto = gerar_id(arquivo_produto);
+
+    fclose(arquivo_produto);
+
+    arquivo_produto = fopen("produtos.csv", "at");
+    if (arquivo_produto == NULL) {
+        printf("\nO arquivo nao foi criado.");
+        getchar();
+        fclose(arquivo_produto);
+    } else {
+        fprintf(arquivo_produto, "%d;", id_produto);
+        fprintf(arquivo_produto, "%s;", nome);
+        fprintf(arquivo_produto, "%s;", valor_produto);
+        fprintf(arquivo_produto, "%s;", tipo_produto);
+        fprintf(arquivo_produto, "%s\n", cor_produto);
+        fclose(arquivo_produto);
+        printf("\n%s cadastrado com sucesso!", nome);
+        printf("\nPressione ENTER para continuar.");
+    }
+    getchar();  // Apenas para pausar antes de sair
 }
+
 
 void exibir_produto(void){
     char id_produto[12];
