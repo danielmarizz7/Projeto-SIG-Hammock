9771cc4ebc109785ca8429038540604e06e714e1
Pedro1349 <pedrolucaspc87@gmail.com>
2025-10-06 16:25:46 -0300

    Feature: Struct cliente criada

--- clientes.h
+++ clientes.h
@@ -1,3 +1,13 @@
+typedef struct cliente Cliente;
+
+struct cliente {
+    int id;
+    char nome[51];
+    char cpf[12];
+    char email[26];
+    char telefone[12];
+};
+
 void modulo_clientes(void);
 char tela_de_clientes(void);
 void cadastrar_clientes(char[], char[], char[], char[]);
