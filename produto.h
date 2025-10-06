da53bc408115c926f26deda320c304457dc2ea7b
danielmarizz7 <daniel.mariz.700@ufrn.edu.br>
2025-10-06 17:41:39 -0300

    Update: Implementa o struct no cabeçalho

--- produto.h
+++ produto.h
@@ -1,6 +1,19 @@
-void modulo_produto(void);
+typedef struct  produto Produto;
+
+struct produto{
+    int id;
+    char modelo_rede[31];
+    char valor_rede[11];
+    char tipo_rede[15]; 
+    char cor_rede [26];
+};
+
+
+
+
 char tela_de_produto(void);
-void cadastrar_produto(char[], char[], char[], char[]);
-void exibir_produto(char[], char[], char[], char[]);
-void alterar_produto(void);
-void excluir_produto(void);
+void cadastrar_produto(Produto*);
+void exibir_produto(Produto*);
+void alterar_produto(Produto*);
+void excluir_produto(Produto*);
+void modulo_produto(void);
