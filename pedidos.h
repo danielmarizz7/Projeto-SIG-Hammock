160a469b7fb32303264e6d4a4b27e7047e196b30
Pedro1349 <pedrolucaspc87@gmail.com>
2025-10-06 16:13:30 -0300

    Feature: Struct pedido criada

--- pedidos.h
+++ pedidos.h
@@ -1,3 +1,14 @@
+typedef struct pedido Pedido;
+
+struct pedido {
+    int id_pedido;
+    int id_cliente;
+    int id_produto;
+    int id_funcionario;
+    float preco;
+    char data[26];
+};
+
 void modulo_pedidos(void);
 char tela_de_pedidos(void);
 void cadastrar_pedidos(int, int, int, float, char[]);
