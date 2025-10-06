bf170a9b67d9f75b518b51e903a76b7ba20aca65
danielmarizz7 <daniel.mariz.700@ufrn.edu.br>
2025-10-06 13:48:38 -0300

    Update: Passa a usar o struct como estrutura

--- funcionarios.h
+++ funcionarios.h
@@ -1,6 +1,19 @@
+typedef struct funcionarios Funcionarios;
+
+struct funcionarios {
+    int id;
+    char nome[51];
+    char cpf[12];
+    char email[26];
+    char telefone[12];
+};
+
+
+
+
 void modulo_funcionarios(void);
-char tela_de_funcionarios(void);
-void cadastrar_funcionarios(char[], char[], char[], char[]);
-void exibir_funcionarios(char[], char[], char[], char[]);
-void alterar_funcionarios(void);
+char tela_de_funcionarios(Funcionarios*);
+void cadastrar_funcionarios(Funcionarios*);
+void exibir_funcionarios(Funcionarios*);
+void alterar_funcionarios(Funcionarios*);
 void excluir_funcionarios(void);
