50625cc701edfdb001a5e7872d07f0c0f31b686d
Pedro1349 <137231370+Pedro1349@users.noreply.github.com>
2025-10-07 16:25:23 -0300

    Add files via upload

--- utilidades.c
+++ utilidades.c
@@ -25,3 +25,10 @@ int gerar_id(FILE *arquivo) {
     id += 1;
     return id;
 }
+
+//fseek() -> joga o ponteiro para o final do arquivo
+//ftell() -> mostra em qual posição do arquivo eu estou
+// Usa o fsize() para pegar o tamanho do struct em questão
+// divide o numero total de bytes pego em ftell() pelo numero obtido por fsize().
+// Assim, dá para saber quantos registros tem no arquivo.
+//modificar o gerar_id
